cc80f6b3b2e5959d95e1a8804bb40a7a1119b5e5
Petr <ptrklsnkv@gmail.com>
2022-06-03 13:40:09 +0300

    app: done. Начинаю готовить скрипты для деплоя

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = express()
 app.use(express.json({extended: true}))
 app.use('/api/auth', require('./routes/auth.routes')) // можно и так делать чтобы не плодить переменные
 app.use('/api/link', require('./routes/link.routes'))
+app.use('/t', require('./routes/redirect.routes'))
 
 const PORT = config.get('port') || 4000
 
--- client/src/components/LinkCard.jsx
+++ client/src/components/LinkCard.jsx
@@ -10,7 +10,7 @@ const LinkCard = ({link}) => {
     <>
       <h2>Ссылка</h2>
 
-      <p>Сокращенная ссылка: <a href={link.to} target='_blank' rel='noopener noreferrer'>{link.to}</a> </p>
+      <p>Ваша ссылка: <a href={link.to} target='_blank' rel='noopener noreferrer'>{link.to}</a> </p>
       <p>Откуда: <a href={link.from} target='_blank' rel='noopener noreferrer'>{link.from}</a> </p>
       <p>Количество кликов по ссылке: <strong>{link.clicks}</strong></p>
       <p>Дата создания: <strong>{new Date(link.date).toLocaleDateString()}</strong></p>
