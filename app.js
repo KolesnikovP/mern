57e7d50bf44a330d96d9f252c05bf1c1c9e353ef
Petr <ptrklsnkv@gmail.com>
2022-06-04 13:10:30 +0300

    гит игнор не отработал и залил модули. Перезаливаю

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const config = require('config')
 const mongoose = require('mongoose')
+const path = require('path')
 
 const app = express()
 
@@ -8,6 +9,17 @@ app.use(express.json({extended: true}))
 app.use('/api/auth', require('./routes/auth.routes')) // можно и так делать чтобы не плодить переменные
 app.use('/api/link', require('./routes/link.routes'))
 app.use('/t', require('./routes/redirect.routes'))
+
+
+/* у нас есть скрипт который запускает только ноду и нам надо подвязать еще и фронт.
+* Для этого нам надо отдавать еще и статику */
+if (process.env.NODE_ENV === 'production') {
+  app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+
+  app.get('*', (req, res)=>{
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 const PORT = config.get('port') || 4000
 
